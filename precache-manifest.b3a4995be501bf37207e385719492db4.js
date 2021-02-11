c754b941dccc6a2d3e8df25fd74a1e2c01981c70
alenzhasulan <alenzhasulan@gmail.com>
2021-02-12 01:11:22 +0600

    Updates

---
+++ precache-manifest.b3a4995be501bf37207e385719492db4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1798103f230101cd805727b31f173c",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "1c0fed51faadac3ea2c3",
+    "url": "/chatbot/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "891e20b94b6fd47b0178",
+    "url": "/chatbot/static/css/main.67c3a66b.chunk.css"
+  },
+  {
+    "revision": "1c0fed51faadac3ea2c3",
+    "url": "/chatbot/static/js/2.0c306944.chunk.js"
+  },
+  {
+    "revision": "aa8079dffa772948bf44d64478c1900a",
+    "url": "/chatbot/static/js/2.0c306944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891e20b94b6fd47b0178",
+    "url": "/chatbot/static/js/main.5e60163f.chunk.js"
+  },
+  {
+    "revision": "6da786bfa225ae5459b9",
+    "url": "/chatbot/static/js/runtime-main.253fb56a.js"
+  }
+]);
